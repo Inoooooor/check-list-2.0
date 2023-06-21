3fbc554878b1b7431d1dc48d1adb528ad5370878
Inoooooor <111541493+Inoooooor@users.noreply.github.com>
2023-06-21 15:52:05 +0300

    style_for_scroll

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import HDE from './plugin/index'
+import './assets/style.scss'
 
 HDE.on('ready', function () {
   if (HDE.getState().formType !== 'create') {
